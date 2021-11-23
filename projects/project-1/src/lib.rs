bdfb7317413a732fc1d05236a5f4dd1155887298
mu001999 <mu001999@outlook.com>
2021-11-23 22:32:15 +0800

    | Project-1-Part-1 < Outline all the types, methods, etc. necessary to make the tests build

---
+++ projects/project-1/src/lib.rs
@@ -0,0 +1,24 @@
+use std::panic;
+use std::iter::Map;
+
+pub struct KvStore {
+    data: Map<String, String>,
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        panic!();
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
+    }
+}

8baf8a140b64a5a9ada9ed6df03e95ae427fc58b
mu001999 <mu001999@outlook.com>
2021-11-26 14:46:11 +0800

    | Project-1-Extension-1 > Use structopt instead of clap

--- projects/project-1/src/bin/kvs.rs
+++ projects/project-1/src/bin/kvs.rs
@@ -1,53 +1,44 @@
-extern crate clap;
 use std::process::exit;
+use structopt::StructOpt;
 
-use clap::{App, Arg, SubCommand};
+#[derive(StructOpt)]
+#[structopt(name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION")
+)]
+enum Config {
+    Set {
+        #[structopt(value_name = "KEY")]
+        _key: String,
+        #[structopt(value_name = "VALUE")]
+        _value: String,
+    },
+    Get {
+        #[structopt(value_name = "KEY")]
+        _key: String,
+    },
+    Rm {
+        #[structopt(value_name = "KEY")]
+        _key: String,
+    },
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(
-            SubCommand::with_name("set")
-                .arg(
-                    Arg::with_name("key")
-                        .value_name("KEY")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .value_name("VALUE")
-                        .required(true)
-                        .index(2),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("get").arg(
-                Arg::with_name("key")
-                    .value_name("KEY")
-                    .required(true)
-                    .index(1),
-            ),
-        )
-        .subcommand(
-            SubCommand::with_name("rm").arg(
-                Arg::with_name("key")
-                    .value_name("KEY")
-                    .required(true)
-                    .index(1),
-            ),
-        )
-        .get_matches();
+    let config = Config::from_args();
 
-    if matches.subcommand_matches("set").is_some()
-        || matches.subcommand_matches("get").is_some()
-        || matches.subcommand_matches("rm").is_some()
-    {
-        eprintln!("unimplemented");
-        exit(1);
-    }
-
-    exit(1);
+    match config {
+        Config::Set { .. } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Config::Get { .. } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Config::Rm { .. } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    };
 }

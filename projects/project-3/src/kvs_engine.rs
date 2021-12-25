21e840339173f751ab19d23edede5042042d2399
Mu001999 <mu001999@outlook.com>
2021-12-25 12:55:02 +0800

    Update

--- projects/project-3/src/kvs_engine.rs
+++ projects/project-3/src/kvs_engine.rs
@@ -4,6 +4,10 @@ mod sled_kvs_engine;
 pub use kv_store::KvStore;
 pub use sled_kvs_engine::SledKvsEngine;
 
-pub trait KvsEngine {
+use crate::Result;
 
+pub trait KvsEngine {
+    fn set(&mut self, key: String, value: String) -> Result<()>;
+    fn get(&mut self, key: String) -> Result<Option<String>>;
+    fn remove(&mut self, key: String) -> Result<()>;
 }

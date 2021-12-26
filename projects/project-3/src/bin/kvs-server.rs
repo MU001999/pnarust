ccef30aa82a772945528b32b7de6a1baa8c9b23c
Mu001999 <mu001999@outlook.com>
2021-12-26 11:40:12 +0800

    Logging

--- projects/project-3/src/bin/kvs-server.rs
+++ projects/project-3/src/bin/kvs-server.rs
@@ -1,5 +1,8 @@
 use kvs::Result;
 use structopt::StructOpt;
+use slog::{info, slog_o};
+use sloggers::Build;
+use sloggers::terminal::{TerminalLoggerBuilder, Destination};
 
 #[derive(StructOpt)]
 #[structopt(name = "kvs-server",
@@ -15,7 +18,15 @@ struct Config {
 }
 
 fn main() -> Result<()> {
+    let mut builder = TerminalLoggerBuilder::new();
+    builder.destination(Destination::Stderr);
+
+    let logger = builder.build()?;
+
     let config = Config::from_args();
+
+    info!(logger, "kvs-server version: {}", env!("CARGO_PKG_VERSION"));
+    info!(logger, "IP-PORT: {}, ENGINE: {}", &config.addr, config.engine.clone().unwrap_or(String::from("kvs")));
 
     let engine = if config.engine.is_some() {
 

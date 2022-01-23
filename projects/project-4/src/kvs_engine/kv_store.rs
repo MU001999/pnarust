87ad47d3bc76582f1df1725fcb8ccb2f06f06e4c
mu001999 <mu001999@outlook.com>
2022-01-24 00:09:30 +0800

    Implemented lock-free readers!

--- projects/project-4/src/kvs_engine/kv_store.rs
+++ projects/project-4/src/kvs_engine/kv_store.rs
@@ -1,6 +1,8 @@
 use super::KvsEngine;
 use crate::{Command, Error, Result};
-use std::sync::{Arc, RwLock};
+use std::fs::copy;
+use std::sync::atomic::{AtomicPtr, Ordering};
+use std::sync::{Arc, Mutex};
 use std::{
     collections::HashMap,
     fs::{self, File, OpenOptions},
@@ -10,16 +12,26 @@ use std::{
 use walkdir::WalkDir;
 
 const SINGLE_FILE_SIZE: u64 = 1024 * 1024;
+const UNUSED_LIMIT: usize = 1024;
 
-struct KvStoreState {
-    index: HashMap<String, (u64, u64)>,
-    active_nth_file: u64,
+struct KvStoreReader(HashMap<String, (u64, u64)>);
+
+impl KvStoreReader {
+    fn raw_arc(index: HashMap<String, (u64, u64)>) -> *mut Arc<KvStoreReader> {
+        Box::into_raw(Box::new(Arc::new(KvStoreReader(index))))
+    }
+}
+
+struct KvStoreWriter {
+    base_file: u64,
+    active_file: u64,
     active_writer: BufWriter<File>,
     unused: usize,
 }
 
 pub struct KvStore {
-    lock: Arc<RwLock<KvStoreState>>,
+    reader: Arc<AtomicPtr<Arc<KvStoreReader>>>,
+    writer: Arc<Mutex<KvStoreWriter>>,
     path: PathBuf,
 }
 
@@ -36,24 +48,33 @@ impl KvStore {
         let mut index = HashMap::new();
         let mut unused = 0;
 
-        // if no file exists, set active_nth_file 0
-        let active_nth_file = if !path_at(0).exists() {
-            0
-        } else {
-            // scan how many kvs.data.* files in the given dir
-            let mut nfile: u64 = 0;
-            for entry in WalkDir::new(&path).min_depth(1).max_depth(1) {
-                if entry?
-                    .file_name()
-                    .to_string_lossy()
-                    .starts_with("kvs.data.")
-                {
-                    nfile += 1;
-                }
+        // scan how many kvs.data.* files in the given dir
+        let mut nfile: u64 = 0;
+        for entry in WalkDir::new(&path).min_depth(1).max_depth(1) {
+            if entry?
+                .file_name()
+                .to_string_lossy()
+                .starts_with("kvs.data.")
+            {
+                nfile += 1;
             }
+        }
+
+        // if no file exists, set active_file 0
+        let (active_file, base_file) = if nfile == 0 {
+            (0, 0)
+        } else {
+            let mut base = 0;
+            base = loop {
+                if path_at(base).is_file() {
+                    break base;
+                } else {
+                    base += 1;
+                }
+            };
 
             // read each kvs.data.* file
-            for i in 0..nfile {
+            for i in base..base + nfile {
                 let file = File::open(path_at(i))?;
                 let reader = BufReader::new(&file);
 
@@ -80,81 +101,125 @@ impl KvStore {
                 }
             }
 
-            nfile - 1
+            (base + nfile - 1, base)
         };
 
         let active_writer = BufWriter::new(
             OpenOptions::new()
                 .create(true)
                 .append(true)
-                .open(path_at(active_nth_file))?,
+                .open(path_at(active_file))?,
         );
 
-        let state = KvStoreState {
-            index,
-            active_nth_file,
+        let reader = Arc::new(AtomicPtr::new(KvStoreReader::raw_arc(index)));
+
+        let writer = KvStoreWriter {
+            base_file,
+            active_file,
             active_writer,
             unused,
         };
+        let writer = Arc::new(Mutex::new(writer));
 
         Ok(KvStore {
-            lock: Arc::new(RwLock::new(state)),
+            reader,
+            writer,
             path,
         })
+    }
+
+    fn get_reader(&self) -> Arc<KvStoreReader> {
+        unsafe {
+            Arc::clone(&(*self.reader.load(Ordering::Relaxed)))
+        }
+    }
+
+    fn swap_index(&self, index: HashMap<String, (u64, u64)>) {
+        let old = self.reader.swap(KvStoreReader::raw_arc(index), Ordering::Relaxed);
+        unsafe {
+            drop(Box::from_raw(old));
+        }
     }
 
     fn path_at(&self, n: u64) -> PathBuf {
         self.path.join("kvs.data.".to_owned() + &n.to_string())
     }
 
-    fn active_path(&self, state: &KvStoreState) -> PathBuf {
-        self.path_at(state.active_nth_file)
+    fn active_path(&self, writer: &KvStoreWriter) -> PathBuf {
+        self.path_at(writer.active_file)
     }
 
     // rewrite records to the active file
-    fn compact(&self, state: &mut KvStoreState) -> Result<()> {
+    fn compact(&self, writer: &mut KvStoreWriter) -> Result<()> {
+        let base_file = writer.base_file;
+        let active_file = writer.active_file;
+
+        // if kvs.data.0 is used, use active file directly
+        // else write keys and values to kvs.data.0
+        let target_file = if base_file == 0 {
+            active_file
+        } else {
+            0
+        };
+
+        let reader = self.get_reader();
         let mut new_index = HashMap::new();
-        for (key, (n, mut pos)) in &state.index {
-            if *n < state.active_nth_file {
-                let command = KvStore::read_command_from(self.path_at(*n), pos)?;
-                pos = KvStore::write_command_to_writer(&mut state.active_writer, &command)?;
+        for (key, (n, pos)) in &reader.0 {
+            // in compact, n < active_file
+            let command = KvStore::read_command_from(self.path_at(*n), *pos)?;
+            let pos = KvStore::write_command_to_writer(&mut writer.active_writer, &command)?;
+            new_index.insert(key.clone(), (target_file, pos));
+        }
+        writer.active_writer.flush()?;
+
+        // copy active file to kvs.data.0 if kvs.data.0 is removed
+        if base_file != 0 {
+            copy(self.active_path(writer), self.path_at(target_file))?;
+        }
+
+        // swap and drop the index in reader
+        self.swap_index(new_index);
+
+        // remove old files (safe by file-rc in OS)
+        // TODO: When other reads occur after getting index,
+        // it may cause old files to be deleted before reading from them
+        if base_file == 0 {
+            for i in 0..active_file {
+                fs::remove_file(self.path_at(i))?;
             }
-
-            new_index.insert(key.clone(), (0, pos));
+        } else {
+            for i in base_file..=active_file {
+                fs::remove_file(self.path_at(i))?;
+            }
         }
 
-        for i in 0..state.active_nth_file {
-            fs::remove_file(self.path_at(i))?;
-        }
-        fs::rename(self.active_path(state), self.path_at(0))?;
-
-        state.index = new_index;
-        state.active_nth_file = 0;
-        state.active_writer = BufWriter::new(
+        // update fields in writer
+        writer.base_file = target_file;
+        writer.active_file = target_file;
+        writer.active_writer = BufWriter::new(
             OpenOptions::new()
                 .create(true)
                 .append(true)
-                .open(self.active_path(state))?,
+                .open(self.active_path(writer))?,
         );
-        state.unused = 0;
+        writer.unused = 0;
 
         Ok(())
     }
 
-    fn try_compact(&self, last_pos: u64, state: &mut KvStoreState) -> Result<()> {
+    fn try_compact(&self, last_pos: u64, writer: &mut KvStoreWriter) -> Result<()> {
         if last_pos > SINGLE_FILE_SIZE {
             // create new file if the active file is large
-            state.active_nth_file += 1;
-            state.active_writer = BufWriter::new(
+            writer.active_file += 1;
+            writer.active_writer = BufWriter::new(
                 OpenOptions::new()
                     .create(true)
                     .write(true)
-                    .open(self.active_path(state))?,
+                    .open(self.active_path(writer))?,
             );
 
-            if state.unused > 1024 {
-                // compact logs if active records are much less than old records
-                self.compact(state)?;
+            if writer.unused > UNUSED_LIMIT {
+                self.compact(writer)?;
             }
         }
         Ok(())
@@ -190,7 +255,8 @@ impl KvStore {
 impl Clone for KvStore {
     fn clone(&self) -> Self {
         KvStore {
-            lock: Arc::clone(&self.lock),
+            reader: Arc::clone(&self.reader),
+            writer: Arc::clone(&self.writer),
             path: self.path.clone(),
         }
     }
@@ -219,21 +285,24 @@ impl KvsEngine for KvStore {
     /// # }
     /// ```
     fn set(&self, key: String, value: String) -> Result<()> {
-        let mut state = self.lock.write().unwrap();
+        let mut writer = self.writer.lock().unwrap();
 
         let command = Command::Set {
             key: key.clone(),
             value,
         };
-        let pos = KvStore::write_command_to_writer(&mut state.active_writer, &command)?;
-        state.active_writer.flush()?;
+        let pos = KvStore::write_command_to_writer(&mut writer.active_writer, &command)?;
+        writer.active_writer.flush()?;
 
-        let active_nth_file = state.active_nth_file;
-        if state.index.insert(key, (active_nth_file, pos)).is_some() {
-            state.unused += 1;
+        let active_file = writer.active_file;
+        let mut new_index = self.get_reader().0.clone();
+        if new_index.insert(key, (active_file, pos)).is_some() {
+            writer.unused += 1;
         }
 
-        self.try_compact(pos, &mut state)?;
+        self.swap_index(new_index);
+
+        self.try_compact(pos, &mut writer)?;
 
         Ok(())
     }
@@ -258,8 +327,7 @@ impl KvsEngine for KvStore {
     /// # }
     /// ```
     fn get(&self, key: String) -> Result<Option<String>> {
-        let state = self.lock.read().unwrap();
-        if let Some(&(n, pos)) = state.index.get(&key) {
+        if let Some(&(n, pos)) = self.get_reader().0.get(&key) {
             match KvStore::read_command_from(self.path_at(n), pos)? {
                 Command::Set { key: _, value } => Ok(Some(value)),
                 _ => Err(Error::ErrorLogMeet),
@@ -289,20 +357,25 @@ impl KvsEngine for KvStore {
     /// # }
     /// ```
     fn remove(&self, key: String) -> Result<()> {
-        let mut state = self.lock.write().unwrap();
+        let mut writer = self.writer.lock().unwrap();
 
-        match state.index.remove(&key) {
-            Some(_) => {
-                let command = Command::Rm { key: key.clone() };
-                let pos = KvStore::write_command_to_writer(&mut state.active_writer, &command)?;
-                state.active_writer.flush()?;
+        let reader = self.get_reader();
+        if reader.0.contains_key(&key) {
+            let mut new_index = reader.0.clone();
+            new_index.remove(&key);
 
-                state.unused += 1;
-                self.try_compact(pos, &mut state)?;
+            let command = Command::Rm { key};
+            let pos = KvStore::write_command_to_writer(&mut writer.active_writer, &command)?;
+            writer.active_writer.flush()?;
+            writer.unused += 1;
 
-                Ok(())
-            }
-            None => Err(Error::KeyNotFound),
+            self.swap_index(new_index);
+
+            self.try_compact(pos, &mut writer)?;
+
+            Ok(())
+        } else {
+            Err(Error::KeyNotFound)
         }
     }
 }

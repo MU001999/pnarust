2efa6320a80cb2d1cf2b64021841d4a2bc062a88
mu001999 <mu001999@outlook.com>
2022-01-30 23:45:31 +0800

    Add comments

--- projects/project-4/src/thread_pool.rs
+++ projects/project-4/src/thread_pool.rs
@@ -7,8 +7,13 @@ pub use naive_thread_pool::NaiveThreadPool;
 pub use rayon_thread_pool::RayonThreadPool;
 pub use shared_queue_thread_pool::SharedQueueThreadPool;
 
+/// A simple trait of thread pools,
+/// which only provides methods new and spawn.
 pub trait ThreadPool: Sized {
+    /// Creates a new thread pool capable of executing `threads` number of jobs concurrently.
     fn new(threads: usize) -> Result<Self>;
+
+    /// Executes the function `job` on one thread in this pool.
     fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static;
